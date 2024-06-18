93cebdb592a6bbbf7475be79ce27a2054cb2a622
Andrey Custódio da Silva <csilvaandrey2006@gmail.com>
2024-06-18 11:18:35 -0300

    projeto cadastro de produtos crud feito em react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+import CadastroProdutos from "./components/CadastroProdutos";
+
+function App() {
+
+
+   return (
+
+    <CadastroProdutos></CadastroProdutos> 
+ 
+   );
+}
+
+export default App;
